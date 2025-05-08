6d33d93b193e065413def4af3325b1003b751f58
mario <mario.wukovits@proton.me>
2025-05-09 01:21:35 +0200

    Algorithm is now iterative instead of recursive

--- src/parse.rs
+++ src/parse.rs
@@ -2,50 +2,64 @@ use std::path::PathBuf;
 use tree_sitter::{Node, Parser};
 
 pub fn create_tree(lang: String, dir: PathBuf) {
-	let mut parser = Parser::new();
-	// TODO: implement better language chooser
-	parser.set_language(&tree_sitter_rust::LANGUAGE.into()).expect("Error loading Rust grammar!");
+    let mut parser = Parser::new();
+    // TODO: implement better language chooser
+    parser
+        .set_language(&tree_sitter_rust::LANGUAGE.into())
+        .expect("Error loading Rust grammar!");
 
-	let content = std::fs::read_to_string(dir).expect("File not found!");
-	let tree = parser.parse(&content, None);
-	println!("{:?}", tree.as_ref().unwrap().root_node().to_sexp());
-	extract_actual_structure(tree.unwrap().root_node(), content);
+    let content = std::fs::read_to_string(dir).expect("File not found!");
+    let tree = parser.parse(&content, None);
+    // println!("{:?}", tree.as_ref().unwrap().root_node().to_sexp());
+    extract_actual_structure(tree.unwrap().root_node(), content);
 }
 
-
+// TODO: Find a way to make this faster
 fn extract_actual_structure(root: Node, content: String) {
     let mut cursor = root.walk();
+    let mut found;
 
-    for function in root.children(&mut cursor).filter(|n| n.kind() == "function_item") {
+    for function in root
+        .children(&mut cursor)
+        .filter(|n| n.kind() == "function_item")
+    {
         let name_node = function.child_by_field_name("name").unwrap();
         let body_node = function.child_by_field_name("body").unwrap();
         let function_name = &content[name_node.byte_range()];
 
-        let mut found_call = false;
+        found = false;
 
-        visit_node_recursive(body_node, &content, &mut |call_node| {
-			if let Some(called_fn_node) = call_node.child_by_field_name("function") {
-				let called_fn_name = &content[called_fn_node.byte_range()];
-				println!("{function_name} calls {called_fn_name}");
-			}
-		});
-		
+        visit_node_iterative(body_node, &content, |call_node| {
+            if let Some(called_fn_node) = call_node.child_by_field_name("function") {
+                let called_fn_name = &content[called_fn_node.byte_range()];
+                found = true;
+                println!("{function_name} calls {called_fn_name}");
+            }
+        });
 
-        // Du kannst hier auch ein Flag setzen, falls kein Aufruf gefunden wurde
+        if !found {
+            println!("{function_name} does not call any function");
+        }
     }
 }
 
-fn visit_node_recursive<F>(node: Node, content: &str, on_call: &mut F)
+
+fn visit_node_iterative<F>(start_node: Node, content: &str, mut on_call: F)
 where
     F: FnMut(Node),
 {
-    if node.kind() == "call_expression" {
-        on_call(node);
-    }
+    let mut stack = vec![start_node];
 
-    for i in 0..node.child_count() {
-        if let Some(child) = node.child(i) {
-            visit_node_recursive(child, content, on_call);
+    while let Some(node) = stack.pop() {
+        if node.kind() == "call_expression" {
+            on_call(node);
+        }
+
+        // Push alle Kinder auf den Stack
+        for i in (0..node.child_count()).rev() {
+            if let Some(child) = node.child(i) {
+                stack.push(child);
+            }
         }
     }
 }
--- test_files/test_1.rs
+++ test_files/test_1.rs
@@ -5,3 +5,8 @@ fn print_to_terminal() {
 fn call_a_fn() {
 	print_to_terminal();
 }
+
+fn test_test() {
+	call_a_fn();
+	print_to_terminal();
+}
