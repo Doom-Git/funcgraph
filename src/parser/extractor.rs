9c38b875cb001cf2631674168fb5df2423feb50d
mario <mario.wukovits@proton.me>
2025-07-27 18:58:11 +0200

    fixed class referencing bug

--- src/parser/extractor.rs
+++ src/parser/extractor.rs
@@ -15,15 +15,15 @@ pub fn extract_structure<T: LanguageSpec>(
     for node in root.children(&mut cursor) {
         if T::is_function_node(&node) {
             let name = T::get_node_name(&node, content).unwrap().to_string();
-            map.entry(class_name.clone() + &name).or_default();
+            map.entry(class_name.clone() + &name);
 
             if let Some(body) = T::get_node_body(&node) {
                 visit_node_iterative(body, |n| {
                     if T::is_call_expression(&n) {
                         if let Some(called) = T::get_called_function_name(&n, content) {
-                            map.entry(class_name.clone() + &name)
+                            map.entry(format!("{}{}", class_name, name))
                                 .or_default()
-                                .push(called.to_string());
+                                .push(format!("{}{}", class_name, called));
                         }
                     }
                 });
